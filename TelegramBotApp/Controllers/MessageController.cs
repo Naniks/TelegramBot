b8b675c4b9727e059aef3f6a255c6d9219268efb
Naniks <nanevokshonov@mail.ru>
2018-02-26 23:18:03 +0300

    create project

---
+++ TelegramBotApp/Controllers/MessageController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Telegram.Bot.Types;
+using TelegramBotApp.Models;
+using TelegramBotApp.Models.Commands;
+
+namespace TelegramBotApp.Controllers
+{
+    public class MessageController : ApiController
+    {
+        [Route(@"api/message/update")]  //web hook uri part
+        public async Task<OkResult> Update([FromBody]Update update)
+        {
+            var commands = Bot.Commands;
+            var message = update.Message;
+            var client = await Bot.Get();
+
+            foreach (var command in commands)
+            {
+                if (command.Contains(message.Text))
+                {
+                    command.Execute(message, client);
+                    break;
+                }
+            }
+
+            HelloCommand commandEx = new HelloCommand();
+            commandEx.Execute(message, client);
+
+
+            return Ok();
+        }
+    }
+}
---
+++ TelegramBotApp/Models/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBotApp.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://telegrambotappnvk.azurewebsites.net/";   //ref to bot
+
+        //public static string Name { get; set; } = "Nivk_bot";  //name of contact
+
+        public static string Key { get; set; } = "340903975:AAHeQ2yYJ-p3tg9dhOE-zesKBTZrSJJ2QGc"; //API key of bot
+    }
+}
